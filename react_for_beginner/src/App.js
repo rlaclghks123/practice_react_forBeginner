d5bf64f21d8885ed53ea89c0007062bf585ce98a
Chi Hwan Kim <rlaclghks123@naver.com>
2022-04-08 14:50:53 +0900

    toDo List
    ...Array 를통해 Array의 요소들을 가져오는 방법을 습득. ex) [4,...[1,2,3]] =>[1,2,3,4]

--- react_for_beginner/src/App.js
+++ react_for_beginner/src/App.js
@@ -1,24 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("Hi!");
-    return () => { console.log("Bye!") };
-  })
-  return (
-    <h1>Hello!</h1>
-  )
-}
+
 
 function App() {
-  const [showing, setShowing] = useState(false);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
 
-  const onClick = () => setShowing((prev) => !prev);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  }
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  }
+
+  console.log(toDo);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>{toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} type="text" onChange={onChange} placeholder="Write To Do"></input>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
