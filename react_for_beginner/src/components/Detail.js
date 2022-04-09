dee9b8fc8982c68af8e3729c4de7f0569cc6c800
Chi Hwan Kim <rlaclghks123@naver.com>
2022-04-09 21:17:42 +0900

    Detail작성2 마무리

--- react_for_beginner/src/components/Detail.js
+++ react_for_beginner/src/components/Detail.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 function Detail() {
     const { id } = useParams();
     const getMovie = async () => {
@@ -7,19 +7,38 @@ function Detail() {
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
         console.log(json);
-
+        setInfos(json.data.movie);
+        setLoading(false);
     };
 
     useEffect(() => {
         getMovie();
     }, []);
 
-
+    const [loading, setLoading] = useState(true);
+    const [infos, setInfos] = useState("");
 
     return (
         <div>
-
-        </div>
+            <div><h3><Link to={`/`}>Home</Link></h3></div>
+            {loading ? <h1>loading...</h1> :
+                <div>
+                    <img src={infos.background_image} alt={infos.title} />
+                    <h3>{infos.title}</h3>
+                    <span>rating :{infos.rating}</span>
+                    <div>
+                        <h3>Genres</h3>
+                        <ul>
+                            {infos.genres.map((g) => <li key={g}>{g}</li>)}
+                        </ul>
+                    </div>
+                    <div>
+                    </div>
+                    <h1>Summary</h1>
+                    <p>{infos.description_full}</p>
+                </div>
+            }
+        </div >
     )
 
 
