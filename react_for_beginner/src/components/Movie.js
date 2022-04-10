86b4252c41d6e681d0110d0fa432653e44b5cee2
Chi Hwan Kim <rlaclghks123@naver.com>
2022-04-10 18:49:38 +0900

    css작업 3 - Movie

--- react_for_beginner/src/components/Movie.js
+++ react_for_beginner/src/components/Movie.js
@@ -1,13 +1,23 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import style from "../css/Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, year, coverImg, title, summary, genres }) {
     return (
-        <div>
-            <img alt={title} src={coverImg} />
-            <h3><Link to={`/movie/${id}`}>{title}</Link></h3>
-            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-            <ul>{genres.map((g) => { return <li key={g}>{g}</li> })}</ul>
+        <div className={style.movie}>
+            <img src={coverImg} alt={title} className={style.movie__img} />
+            <div>
+                <h2 className={style.movie__title}>
+                    <Link to={`/movie/${id}`}>{title}</Link>
+                </h2>
+                <h3 className={style.movie__year}>{year}</h3>
+                <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+                <ul className={style.movie__genres}>
+                    {genres.map((g) => (
+                        <li key={g}>{g}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 };
